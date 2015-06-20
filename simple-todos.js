264ba6107964fed0e9b1dc282feabce362b19499
JD Maresco <jdmaresco@gmail.com>
2015-06-20 18:08:17 -0400

    Add checkbox and delete functionality

--- simple-todos.js
+++ simple-todos.js
@@ -25,4 +25,13 @@ if (Meteor.isClient) {
     }
   })
 
+  Template.task.events({
+    "click .toggle-checked": function () {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  })
+
 }
